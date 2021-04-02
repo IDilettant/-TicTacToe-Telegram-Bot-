0160fb7a346d352fbd3eb44c4424f7beb7fc8b2b
idilettant <dedenev1985@gmail.com>
2021-04-02 20:34:32 +0300

    Add printing board function

--- xobot/scripts/xobot.py
+++ xobot/scripts/xobot.py
@@ -3,6 +3,14 @@
 
 PLAYER_X = 1
 PLAYER_O = 2
+DIMENSION = 3
+board_state = [[None for row in range(DIMENSION)] for col in range(DIMENSION)]
+moves = []
+marker_to_char = {
+    None: ' . ',
+    PLAYER_X: ' x ',
+    PLAYER_O: ' o ',
+}
 
 
 def switch_player(player):
@@ -17,9 +25,22 @@ def switch_player(player):
     return PLAYER_X if player == PLAYER_O else PLAYER_O
 
 
+def show_board(current_state):
+    """Print current state of board.
+
+    Args:
+        current_state (list): board current state
+    """
+    for row in range(DIMENSION):
+        line = []
+        for col in range(DIMENSION):
+            line.append(marker_to_char[current_state[row][col]])
+        print(''.join(line))
+
+
 def main():
     """Create main function."""
-    pass  # noqa: WPS420
+    show_board(board_state)  # noqa: WPS420
 
 
 if __name__ == '__main__':
