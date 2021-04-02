042e0d68395bcda26e91ab2f064503fc085e1609
idilettant <dedenev1985@gmail.com>
2021-04-02 20:03:34 +0300

    Add players and player switching function

--- xobot/scripts/xobot.py
+++ xobot/scripts/xobot.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python
 """Basic module for bot logic."""
 
+PLAYER_X = 1
+PLAYER_O = 2
+
+
+def switch_player(player):
+    """Change current player.
+
+    Args:
+        player: current player
+
+    Returns:
+        another player
+    """
+    return PLAYER_X if player == PLAYER_O else PLAYER_O
+
 
 def main():
     """Create main function."""
