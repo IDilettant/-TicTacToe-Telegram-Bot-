705b7eaf9204e2174bd0417cb1ab4fef95557343
idilettant <dedenev1985@gmail.com>
2021-04-03 14:32:05 +0300

    Refactor function vars names

--- tictactoe/scripts/player.py
+++ tictactoe/scripts/player.py
@@ -17,18 +17,20 @@ def switch_player(player):
     return x_char if player == o_char else o_char
 
 
-def make_move(current_state, row, col, player):
+def make_move(current_state, row, col, player_mark):
     """Change board current state based on the move made.
 
     Args:
         current_state (list): board current state
         row (int): board row index
         col (int): board col index
-        player (int): current player
+        player_mark (int): mark of current player
 
     Returns:
         Board current state
     """
-    if current_state[row][col] in get_legal_moves(current_state):
-        current_state[row][col] = player
+    next_move = current_state[row][col]
+    legal_moves = get_legal_moves(current_state)
+    if next_move in legal_moves:
+        current_state[row][col] = player_mark
     return current_state
