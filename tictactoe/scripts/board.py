d9180d72eee6ff8512a21899ad70050128650c87
idilettant <dedenev1985@gmail.com>
2021-04-08 19:33:00 +0300

    Rewrite module with Board class

--- tictactoe/scripts/board.py
+++ tictactoe/scripts/board.py
@@ -1,140 +1,74 @@
 """Board module."""
-import tictactoe.scripts.player as player
-
-SIDE_SIZE = 3
 
 
-def show_state(current_state):
-    """Print current state of board.
+class Board(object):
+    """Game board class."""
 
-    Args:
-        current_state (list): board current state
-    """
-    markers_cells = {
-        None: ' . ',
-        player.x_char: ' x ',
-        player.o_char: ' o ',
-    }
-    for row in range(SIDE_SIZE):
-        line = []
-        for col in range(SIDE_SIZE):
-            line.append(markers_cells[current_state[row][col]])
-        print(''.join(line))
+    def __init__(self):
+        """Build a class constructor.
 
+        The constructor takes the side size,
+        the basic state of the board and a list of moves
+        """
+        self.side_size = 3
+        self.current_state = {
+            cell: ' . ' for cell in range(self.side_size ** 2)
+        }
+        self.moves_made = []
 
-def has_row_win(current_state):
-    """Check for a win horizontally.
+    def get_grid(self):
+        """Get a grid with chars according the current state of game board.
 
-    Args:
-        current_state (list): board current state
+        Returns:
+            Board grid
+        """
+        keys = list(self.current_state.keys())
+        return [
+            [
+                self.current_state[keys.pop(0)] for _ in range(self.side_size)
+            ] for _ in range(self.side_size)
+        ]
 
-    Returns:
-        bool
-    """
-    for row in range(SIDE_SIZE):
-        unique_rows = set(current_state[row])
-        if len(unique_rows) == 1:
-            if unique_rows.pop() is not None:
-                return True
-    return False
+    def make_move(self, move, player_char):
+        """Change board current state based on the move made.
 
+        Args:
+            move (int): coordinate of board cell
+            player_char (str): char of current player
 
-def has_col_win(current_state):
-    """Check for a win vertically.
+        Returns:
+            Board current state
+        """
+        legal_moves = [
+            cell for cell, char in self.current_state.items() if char == ' . '
+        ]
+        if move in legal_moves:
+            self.current_state[move] = player_char
+        self.moves_made.append(move)
+        return self.current_state
 
-    Args:
-        current_state (list): board current state
+    def has_win(self):
+        """Check for the win.
 
-    Returns:
-        bool
-    """
-    for col in range(SIDE_SIZE):
-        unique_cols = set()
-        for row in range(SIDE_SIZE):
-            unique_cols.add(current_state[row][col])
-        if len(unique_cols) == 1:
-            if unique_cols.pop() is not None:
-                return True
-    return False
-
-
-def has_diagonal_win(current_state):
-    """Check for a win diagonally.
-
-    Args:
-        current_state (list): board current state
-
-    Returns:
-        bool
-    """
-    # check backwards diagonal (top left to bottom right)
-    backwards_diag = set()
-    backwards_diag.add(current_state[0][0])
-    backwards_diag.add(current_state[1][1])
-    backwards_diag.add(current_state[2][2])
-    if len(backwards_diag) == 1:
-        if backwards_diag.pop() is not None:
+        Returns:
+            bool
+        """
+        grid = self.get_grid()
+        backwards_diag = [grid[cell][cell] for cell in range(self.side_size)]
+        reversed_grid = list(reversed(grid))
+        forwards_diag = [
+            reversed_grid[cell][cell] for cell in range(self.side_size)
+        ]
+        if any(map(self._line_has_match, grid)):
             return True
-    # check forwards diagonal (bottom left to top right)
-    forwards_diag = set()
-    forwards_diag.add(current_state[2][0])
-    forwards_diag.add(current_state[1][1])
-    forwards_diag.add(current_state[0][2])
-    if len(forwards_diag) == 1:
-        if forwards_diag.pop() is not None:
+        elif any(map(self._line_has_match, zip(*grid))):
             return True
-    return False
+        elif self._line_has_match(backwards_diag):
+            return True
+        elif self._line_has_match(forwards_diag):
+            return True
+        return False
 
-
-def has_win(current_state):
-    """Check for the win.
-
-    Args:
-        current_state (list): board current state
-
-    Returns:
-        bool
-    """
-    return has_row_win(
-        current_state,
-    ) or has_col_win(
-        current_state,
-    ) or has_diagonal_win(
-        current_state,
-    )
-
-
-def get_legal_moves(current_state):
-    """Get coordinates of possible moves.
-
-    Args:
-        current_state (list): board current state
-
-    Returns:
-        Coordinates of possible moves
-    """
-    possible_choices = []
-    for row in range(SIDE_SIZE):
-        for col in range(SIDE_SIZE):
-            # Check a cell is empty
-            if current_state[row][col] is None:
-                possible_choices.append((row, col))
-    return possible_choices
-
-
-def make_move(current_state, next_move, player_mark):
-    """Change board current state based on the move made.
-
-    Args:
-        next_move (tuple): cell coordinates
-        current_state (list): board current state
-        player_mark (int): mark of current player
-
-    Returns:
-        Board current state
-    """
-    row, col = next_move
-    legal_moves = get_legal_moves(current_state)
-    if next_move in legal_moves:
-        current_state[row][col] = player_mark
-    return current_state
+    def _line_has_match(self, line):
+        line = set(line)
+        return len(line) == 1 and line.pop() != ' . '
