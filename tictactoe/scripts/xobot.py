4d1f5ede70419097672ebe0be42187a0005f4b00
idilettant <dedenev1985@gmail.com>
2021-04-11 15:08:18 +0300

    Add search functions

--- tictactoe/scripts/xobot.py
+++ tictactoe/scripts/xobot.py
@@ -1,4 +1,8 @@
 """Basic module for bot logic."""
+import copy
+from sys import maxsize
+
+from tictactoe.scripts.player import Player
 
 
 class Node(object):
@@ -75,3 +79,83 @@ class XoBot(object):
         if alpha_choice.value >= beta_choice.value:
             return node
         return beta_choice
+
+    def _make_deep_first_search(  # noqa: WPS211 WPS210
+        self,
+        board,
+        current_player,
+        depth,
+        alpha_choice,
+        beta_choice,
+    ):
+        """Search best move for possible moves on game board.
+
+        Args:
+            board: current state of game board
+            current_player: a player which make the current move
+            depth (int): sequential number of the tree level
+            alpha_choice: class instance containing value of upper bound
+            beta_choice: class instance containing value of lower bound
+
+        Returns:
+            Class instance containing best possible move
+        """
+        legal_moves = board.get_legal_moves()
+        for move in legal_moves:
+            new_board = copy.deepcopy(board)
+            new_board.make_move(move, current_player)
+            node = self._find_best_choice(
+                new_board,
+                current_player.switch_turn(),
+                depth + 1,
+                alpha_choice.value,
+                beta_choice.value,
+            )
+            node.move = new_board.last_move
+            if current_player == Player.x_char:
+                alpha_choice = self._make_alpha_pruning(
+                    node, alpha_choice, beta_choice,
+                )
+            else:
+                beta_choice = self._make_beta_pruning(
+                    node, alpha_choice, beta_choice,
+                )
+        return alpha_choice if current_player == Player.x_char else beta_choice
+
+    def _find_best_choice(  # noqa: WPS211
+        self,
+        board,
+        current_player,
+        depth=0,
+        alpha=-maxsize,
+        beta=maxsize,
+    ):
+        """Find best choice from possible moves on game board.
+
+        Args:
+            board: current state of game board
+            current_player: a player which make the current move
+            depth (int): sequential number of the tree level
+            alpha (int): the maximum lower bound of possible solutions
+            beta (int): the minimum upper bound of possible solutions
+
+        Returns:
+            An instance of Choice class
+        """
+        alpha_choice = Node(value=alpha)
+        beta_choice = Node(value=beta)
+
+        if board.has_win():
+            if current_player == Player.x_char:
+                return Node(board.get_last_move(), 10 - depth, depth)
+            return Node(board.get_last_move(), -10 + depth, depth)
+        elif len(board.moves_made) == 9:
+            return Node(board.last_move(), 0, depth)
+
+        return self._make_deep_first_search(
+            board,
+            current_player,
+            depth,
+            alpha_choice,
+            beta_choice,
+        )
