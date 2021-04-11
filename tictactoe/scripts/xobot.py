20667b7c1027402d5c2e13ec8f734a668d702bf4
idilettant <dedenev1985@gmail.com>
2021-04-11 14:57:38 +0300

    Add class to store node state values

--- tictactoe/scripts/xobot.py
+++ tictactoe/scripts/xobot.py
@@ -1,11 +1,17 @@
-#!/usr/bin/env python
 """Basic module for bot logic."""
 
 
-def main():
-    """Run module logic."""
-    pass  # noqa: WPS420
+class Node(object):
+    """Node of tree of possible states of game board."""
 
+    def __init__(self, move=0, value=0, depth=0):  # noqa: WPS110
+        """Build a class instance.
 
-if __name__ == '__main__':
-    main()
+        Args:
+            move (int): coordinate of board cell
+            value (int): outcome significance of board state
+            depth (int): sequential number of the tree level
+        """
+        self.move = move
+        self.value = value  # noqa: WPS110
+        self.depth = depth
